9c4b7bd005f6dd2e4ad174cdc601809ffdcf46fe
Karel Pičman <karel.picman@kontron.com>
2023-11-21 15:20:16 +0100

    Autologin #21

--- app/controllers/redmine_oauth_controller.rb
+++ app/controllers/redmine_oauth_controller.rb
@@ -27,6 +27,7 @@ class RedmineOauthController < AccountController
 
   def oauth
     session[:back_url] = params[:back_url]
+    session[:autologin] = params[:autologin]
     oauth_csrf_token = generate_csrf_token
     session[:oauth_csrf_token] = oauth_csrf_token
     case Setting.plugin_redmine_oauth[:oauth_name]
@@ -98,6 +99,8 @@ class RedmineOauthController < AccountController
   def try_to_login(email, info)
     params['back_url'] = session[:back_url]
     session.delete :back_url
+    params['autologin'] = session[:autologin]
+    session.delete :autologin
     user = User.joins(:email_addresses).where(email_addresses: { address: email }).first
     if user # Existing user
       if user.registered? # Registered
