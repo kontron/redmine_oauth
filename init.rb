215e3ddc8214e537ceba00a58b262bc19ea4c420
Karel Pičman <karel.picman@kontron.com>
2026-02-24 11:54:31 +0100

    v4.0.4 devel started

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :redmine_oauth do
   name 'Redmine OAuth plugin'
   author 'Karel Pičman'
   description 'Redmine OAuth plugin'
-  version '4.0.3'
+  version '4.0.4 devel'
   url 'https://github.com/kontron/redmine_oauth'
   author_url 'https://github.com/kontron/redmine_oauth/graphs/contributors'
 
