4eff8b923000974adb5e70abbc2bdc8dc348556a
Alfredo Rezinovsky <alfrenovsky@gmail.com>
2026-03-05 15:34:02 +0000

    Added standard header to lib/data_flattener.rb

--- lib/data_flattener.rb
+++ lib/data_flattener.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
+
+# Redmine plugin OAuth
+#
+# Karel Pičman <karel.picman@kontron.com>
+#
+# This file is part of Redmine OAuth plugin.
+#
+# Redmine OAuth plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Redmine OAuth plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with Redmine OAuth plugin. If not, see
+# <https://www.gnu.org/licenses/>.
+
 module DataFlattener
   # Shared logic to avoid code repetition between Hash and Array refinements.
   module SharedMethods
