bcf90ae4e6e64a252265ca867d6db65ae4188417
Karel Pičman <karel.picman@kontron.com>
2026-03-27 07:31:33 +0100

    Apply suggestion from @picman

--- db/migrate/20260325120001_oauth_provider_client_id_length.rb
+++ db/migrate/20260325120001_oauth_provider_client_id_length.rb
@@ -13,7 +13,7 @@ class OauthProviderClientIdLength < ActiveRecord::Migration[7.2]
   end
 
   def down
-    change_column :oauth_providers, :site, :string, null: true, limit: 256
+    change_column :oauth_providers, :site, :string, null: true, limit: 40
     change_column :oauth_providers, :client_id, :string, null: false, limit: 80
   end
 end
